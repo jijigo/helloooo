ba1cb8fb6c0c147c5609d22cfc034964c8ea5a71
debbyji <jasmine820921@gmail.com>
2018-01-17 23:03:44 +0800

    revise gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,23 @@
 // 引用 gulp plugin
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var connect = require('gulp-connect');
 
 // 建立預設 gulp task
 gulp.task('default', function () {
     console.log('hello');
 });
+
+// compiler scss
+gulp.task('sass', function () {
+    return gulp.src('./scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+});
+
+// 連接伺服器
+gulp.task('connect', function() {
+    connect.server();
+});
+
+gulp.task('default', ['connect']);
